ed7b6c4cfbb0d86020a69023fb272e03b0ce8bc7
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-18 20:19:01 +0200

    Added: format specifiers

---
+++ lib/format_specifiers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// format specifiers % = defines and formats a type of data to be displayed
+
+
+int main(){
+
+    /*
+        %c = character
+        %s = strings (array of characters)
+        %f = float
+        %lf = double (long float)
+        %d = integer
+
+        %.1 = decimal precision (1 digit)
+        %.2 = decimal precision (2 digits)
+
+        %1 = minimum field width
+        %- = left allign
+    */
+
+    return 0;
+}
